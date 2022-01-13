2766545c96dbd6b7b827329f381eda1bba03b939
Vitalii Maklai <vitalii.maklai@gmail.com>
2022-01-13 15:44:54 -0500

    Startup script: Added outputting config files: "jetty.xml" and "log4j.properties"

--- assets/run.sh
+++ assets/run.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
 python2 /app/entrypoint/Init.py
+
+echo ""
+echo "---"
+echo ""
+
+cat -n /opt/activemq/conf.tmp/jetty.xml
+
+echo ""
+echo "---"
+echo ""
+
+cat -n /opt/activemq/conf.tmp/log4j.properties
+
+echo ""
+echo "---"
+echo ""
+
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
